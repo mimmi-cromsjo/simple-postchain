e3d25beb2a388826cb6a28a8fd38e24301eeb60e
Viktor Plane <viktor.plane@simployer.com>
2021-03-15 15:04:02 +0100

    Update RID retriever script

--- docker/add-rid-to-env.sh
+++ docker/add-rid-to-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-RID=$(docker exec -i testchain cat rell/target/blockchains/1/brid.txt)
+RID=$(docker exec -i testchain cat rell/target/blockchains/0/brid.txt)
 
 echo "Adding RID ${RID} to project .env"
 echo "{ \"brid\": \"${RID}\" }" > ../config.json
